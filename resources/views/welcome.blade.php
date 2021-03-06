0323a6c9f3a48d1375dd9aaca8189ae971a87870
reyhaneh.sp <reyhaneh.sanat@yahoo.co.uk>
2021-03-06 10:20:07 +0330

    commit done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel in development branch start
                 </div>
 
                 <div class="links">
